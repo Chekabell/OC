34393d962e602cca7b41c95659f3e111edada3c7
Chekabell <nikita.toporischev@mail.ru>
2024-03-26 11:01:28 +0500

    1

--- OC.cpp
+++ OC.cpp
@@ -1,32 +1,22 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <random>
+
+void randomGen(int* array, const int len){
+	srand(time(0));
+	for (int i = 0; i < len; i++) {
+		array[i] = rand();
+	}
+}
+
+DWORD WINAPI bubbleSort (){
+
+}
 
 int main(void) {
-	STARTUPINFO si;
-	PROCESS_INFORMATION pi;
-	FILETIME fl1, fl2, fl3, fl4;
-	SYSTEMTIME st1, st2, st3, st4;
+	const int len = 100000;
+	int* array = new int[len];
+	randomGen(array, len);
 
-	ZeroMemory(&si, sizeof(si));
-	ZeroMemory(&pi, sizeof(pi));
-	if (CreateProcess(L"C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) std::cout << "Creating success" << std::endl;
-	else { std::cout << "Creating fail" << std::endl; return 0; };
 
-	WaitForSingleObject(pi.hProcess, INFINITE);
-	GetProcessTimes(pi.hProcess, &fl1, &fl2, &fl3, &fl4);
-
-	FileTimeToSystemTime(&fl1, &st1);
-	FileTimeToSystemTime(&fl2, &st2);
-	FileTimeToSystemTime(&fl3, &st3);
-	FileTimeToSystemTime(&fl4, &st4);
-
-	std::cout << "Time start: " << st1.wMinute << ":" << st1.wSecond << ":" << st1.wMilliseconds << std::endl;
-	std::cout << "Time end: " << st2.wMinute << ":" << st2.wSecond << ":" << st2.wMilliseconds << std::endl;
-	std::cout << "Time in core mode: " << st3.wMinute << ":" << st3.wSecond << ":" << st3.wMilliseconds << std::endl;
-	std::cout << "Time in user mode: " << st4.wMinute << ":" << st4.wSecond << ":" << st4.wMilliseconds << std::endl;
-	std::cout << "Handle: " << pi.hProcess << std::endl;
-
-	CloseHandle(pi.hProcess);
-
-	return 0;
 }
