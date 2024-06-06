e4ad276678ce7ae96a625b632d3423e27f0506b5
Chekabell <nikita.toporischev@mail.ru>
2024-06-06 13:37:41 +0500

    general fix

--- OC.cpp
+++ OC.cpp
@@ -25,16 +25,16 @@ int main(void) {
 	startInfo.hStdInput = handleChildRd;
 	startInfo.dwFlags |= STARTF_USESTDHANDLES;
 
-	std::wstring CommandLine(L"C:\\Users\\User\\source\\repos\\Sub OS\\x64\\Release\\Sub OS.exe");
+	std::wstring CommandLine(L"C:\\WINDOWS\\system32\\notepad.exe");
 	LPWSTR lpwCmdLine = &CommandLine[0];
 
-	if (!CreateProcessW(NULL, lpwCmdLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &startInfo, &processInfo)) {
+	if (!CreateProcessW(NULL, lpwCmdLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL,&startInfo, &processInfo)) {
 		std::cout << "Failed creating sub process" << std::endl;
 	}
 
 	DWORD dwWritten;
 	CHAR buffer[BUFF_SIZE] = "1623623" ;
-	
+
 	for (int i = 0; buffer[i] != '\0'; i++) {
 		std::cout << ((int)buffer[i] - '0') << " ";
 	}
